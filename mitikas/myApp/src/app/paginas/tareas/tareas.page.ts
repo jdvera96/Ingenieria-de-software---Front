ab1863f9bc869eec061ccb3fb8d239e9c8332f70
jdvera96 <jdvera@espol.edu.ec>
2020-01-20 14:34:52 -0500

    bug corregido

--- mitikas/myApp/src/app/paginas/tareas/tareas.page.ts
+++ mitikas/myApp/src/app/paginas/tareas/tareas.page.ts
@@ -4,31 +4,32 @@ import { ConsultaService } from 'src/app/servicio/consulta.service';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-tareas',
-  templateUrl: './tareas.page.html',
-  styleUrls: ['./tareas.page.scss'],
+    selector: 'app-tareas',
+    templateUrl: './tareas.page.html',
+    styleUrls: ['./tareas.page.scss']
 })
 export class TareasPage implements OnInit {
 
-  id:number;
+  id: number;
   datos: any[];
   constructor(public nav: NavController,private activatedRoute: ActivatedRoute,public consulta: ConsultaService) {
-    const num=this.activatedRoute.snapshot.paramMap.get('id_clase');
-    this.id=parseInt(num, 10);
-    consulta.obtenerClase("0911111111",num).subscribe((data)=>{
-      var anydata=<any>data;
-      this.datos = anydata;
-    });
-   }
+      const num=this.activatedRoute.snapshot.paramMap.get('id_clase');
+      this.id=parseInt(num, 10);
+      consulta.obtenerClase('0911111111',num).subscribe(data=>{
+          const anydata=<any>data;
+          this.datos = anydata;
+      });
+  }
 
   ngOnInit() {
   }
 
   segmentChanged(ev: any) {
-    if(ev.detail.value == "Calificaciones")
-      this.nav.navigateRoot(`calificaciones/${this.id}`);
-    else if(ev.detail.value == "Asistencias")
-      this.nav.navigateRoot(`asistencias/${this.id}`);
+      if(ev.detail.value == 'Calificaciones') {
+          this.nav.navigateRoot(`calificaciones/${this.id}`);
+      } else if(ev.detail.value == 'Asistencias') {
+          this.nav.navigateRoot(`asistencias/${this.id}`);
+      }
   }
 
   scriptItem(){
