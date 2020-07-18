b688152e80c3eae0339a6d27529dc547c8b5020c
lindthon <43838005+lindthon@users.noreply.github.com>
2020-07-18 12:03:12 -0500

    Merge and conflicts solved

---
+++ Plataforma/src/app/pages/auth/login/login.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
+import {AuthService} from '../../../servicios/auth.service'
+
+@Component({
+  selector: 'ngx-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+
+
+export class LoginComponent implements OnInit {
+
+  elemento: string;
+  inputText;
+  public correo: string;
+  public password: string;
+  constructor(private servicioApiLogin : AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+
+  logearse(){
+    this.correo=$('#correo').val().toString();
+    this.password=$('#password').val().toString();
+    console.log('valor del correo: ',this.correo);
+    console.log('valor del pass: ',this.password);
+
+    this.verificarCredenciales();
+    
+  }
+
+  verificarCredenciales(){
+      this.servicioApiLogin.obtenerLogin(this.correo,this.password).subscribe(response=>{
+        console.log('resultado: ',response);
+        
+        if(response["rol"]){
+
+          let valor=response["rol"]+"-"+response["nombre"]+" "+response["apellido"];
+          localStorage.setItem('login-mitikas',valor);
+          location.href="http://localhost:4200/";
+        }else{
+          console.log('credenciales invalidas');
+        }
+
+      });
+      
+  }
+
+}
